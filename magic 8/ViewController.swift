197a84a005cd3a2559d3481e45f2ac6915eda145
Thiago Leite <thiago.0686@gmail.com>
2021-02-02 20:15:28 -0300

    Initial Commit

---
+++ magic 8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  magic 8
+//
+//  Created by Thiago Leite on 02/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
